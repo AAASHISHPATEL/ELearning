42eb273868013aab6a6d14a3d6920144fb5dfbf7
ASHISH PATEL <pashishp005@gmail.com>
2026-03-14 15:02:23 +0530

    backend link updated

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    // baseURL: 'https://lmsbackend-zkny.onrender.com' || 'http://localhost:5000',
-    baseURL: 'http://localhost:5000',
+  baseURL: "https://elearning-tfcs.onrender.com" || "http://localhost:5000",
+  // baseURL: 'http://localhost:5000',
 });
 
 axiosInstance.interceptors.request.use((config) => {
